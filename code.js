d2a066c3722327b047150fb3bdacf0dc2dac802a
unknown <patrickh61@yahoo.com>
2021-05-18 12:31:09 -0400

    finished activity

--- code.js
+++ code.js
@@ -2,3 +2,17 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+let submitButton = document.getElementById('submit_button');
+
+submitButton.addEventListener('click', function(event){
+    event.preventDefault()
+    let nameInput = document.getElementById('name_input');
+    let emailInput = document.getElementById('email_input');
+    let messageArea = document.getElementById('message_area');
+
+    let nameInputValue = nameInput.value;
+    let emailInputValue = emailInput.value;
+    let messageAreaValue = messageArea.value;
+
+    alert(`${nameInputValue} sent you a message saying \n'${messageAreaValue}'\nMessage them back at ${emailInputValue}`)
+}) 
